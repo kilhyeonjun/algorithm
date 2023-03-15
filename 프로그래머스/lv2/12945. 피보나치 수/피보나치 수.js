e8feed6cf15f304f83db03f280161c9933a6c6af
kilhyeonjun <kboxstar@gmail.com>
2023-03-15 12:45:34 +0900

    [level 2] Title: 피보나치 수, Time: 4.55 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    const arr = [0, 1];
+    for(let i = 2; i < n; i++) {
+        arr.push((arr[i-1] + arr[i-2]) % 1234567);
+    }
+    return (arr[n-1]  + arr[n-2]) % 1234567;
+}
