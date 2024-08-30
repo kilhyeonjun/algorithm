0d3ed78464c0579c76874e980ea3f008f27611dc
kilhyeonjun <kboxstar@gmail.com>
2024-08-30 12:13:10 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String[] cloth: clothes) map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        for(Integer value: map.values()) answer *= (value + 1);
+        
+        return answer - 1;
+    }
+}
